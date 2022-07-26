2a4c851a87cd765a412805b846074fc1a606791a
Manuel Del Real <manueldelreal@gmail.com>
2022-07-26 07:26:42 +0000

    Added new assets

--- app.py
+++ app.py
@@ -5,16 +5,18 @@ import dash_html_components as html
 import os
 
 ###### Set up variables
-list_of_choices=['punch', 'body-slam', 'round-house kick to the face']
-githublink = 'https://github.com/austinlasseter/chuck_norris_execution'
-image1='chucknorris.jpg'
-heading1='Chuck Norris execution method'
+list_of_choices=['Bulbasaur', 'Squirtle', 'Charmander']
+githublink = 'https://github.com/manueldelreal/201-chuck-norris-callback'
+image1='bulbasaur.png'
+image2='squirtle.png'
+image3='charmander.png'
+heading1='Choose your starter'
 
 ########### Initiate the app
 external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
 app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
 server = app.server
-app.title='Chuck'
+app.title='Pokemon starters'
 
 ####### Layout of the app ########
 app.layout = html.Div([
@@ -22,7 +24,7 @@ app.layout = html.Div([
     html.Img(src=app.get_asset_url(image1), style={'width': 'auto', 'height': '10%'}),
     dcc.Dropdown(id='your-input-here',
                 options=[{'label': i, 'value': i} for i in list_of_choices],
-                value='punch',
+                value='Bulbasaur',
                 style={'width': '500px'}),
     html.Br(),
     html.Div(id='your-output-here', children=''),
@@ -36,7 +38,7 @@ app.layout = html.Div([
 @app.callback(dash.dependencies.Output('your-output-here', 'children'),
               [dash.dependencies.Input('your-input-here', 'value')])
 def display_value(whatever_you_chose):
-    return f'Chuck Norris will now execute you with a {whatever_you_chose}.'
+    return f'Great choice going with a {whatever_you_chose}.'
 
 
 ######### Run the app #########
